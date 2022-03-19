c7e10ec655a96610c4700940eeeca18ff4d8ddb4
pluie <pluie@DESKTOP-K9PKHA7>
2022-03-19 15:15:35 +0900

    첫 커밋, 최소 라이브러리 세팅

---
+++ MilanorTools/src/test/java/com/example/MilanorTool/MilanorToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MilanorTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilanorToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
