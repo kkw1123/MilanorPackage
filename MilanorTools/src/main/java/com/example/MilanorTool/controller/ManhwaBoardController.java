2ff44f8792e142b61bcaf31838dc5b957f4015db
pluie <pluie@DESKTOP-K9PKHA7>
2022-03-20 22:40:37 +0900

    getter setter 및 thymeleaf 연결

--- MilanorTools/src/main/java/com/example/MilanorTool/controller/ManhwaBoardController.java
+++ MilanorTools/src/main/java/com/example/MilanorTool/controller/ManhwaBoardController.java
@@ -1,14 +1,27 @@
 package com.example.MilanorTool.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.MilanorTool.model.ManhwaBoard;
+import com.example.MilanorTool.repository.ManhwaBoardRepository;
 
 @Controller
 @RequestMapping("/manhwa")
 public class ManhwaBoardController {
+	
+	@Autowired
+	private ManhwaBoardRepository boardRepository;
+	
 	@GetMapping("/list")
-	public String list() {
+	public String list(Model model) {
+		List<ManhwaBoard> boards = boardRepository.findAll();
+		model.addAttribute("boards", boards);
 		return "manhwa/list";
 	}
 }
--- MilanorTools/src/main/java/com/example/MilanorTool/model/ManhwaBoard.java
+++ MilanorTools/src/main/java/com/example/MilanorTool/model/ManhwaBoard.java
@@ -10,10 +10,29 @@ import lombok.Data;
 @Entity
 @Data
 public class ManhwaBoard {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String content;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
