594ebfad87d508a6f098a818baa17071f5a3b2ca
pluie <pluie@DESKTOP-K9PKHA7>
2022-03-23 23:09:34 +0900

    zip압축파일 안의 이미지를 불러오는 로직 추가

--- MilanorTools/src/main/java/com/example/MilanorTool/controller/HomeController.java
+++ MilanorTools/src/main/java/com/example/MilanorTool/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.example.MilanorTool.controller;
 
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.MilanorTool.model.Manhwa;
 import com.example.MilanorTool.repository.ManhwaRepository;
+import com.example.MilanorTool.utils.Zip_tools;
 
 @Controller
 public class HomeController {
@@ -17,9 +26,12 @@ public class HomeController {
 	private ManhwaRepository boardRepository;
 	
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model) throws IOException {
 		List<Manhwa> manhwa = boardRepository.findAll();
+		InputStream inputStream = new ByteArrayInputStream(Zip_tools.Zip_tools_byte());
+		byte[] byteArray = IOUtils.toByteArray(inputStream);
 		model.addAttribute("manhwa", manhwa);
+		model.addAttribute("image", Base64.getEncoder().encodeToString(byteArray));
 		return "index";
 	}
 }
--- MilanorTools/src/main/java/com/example/MilanorTool/utils/Zip_tools.java
+++ MilanorTools/src/main/java/com/example/MilanorTool/utils/Zip_tools.java
@@ -1,5 +1,85 @@
 package com.example.MilanorTool.utils;
 
-public class Zip_tools {
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+import javax.imageio.ImageIO;
+
+
+public class Zip_tools {
+	
+	public static byte[] Zip_tools_byte() throws IOException {
+		ZipFile zip = new ZipFile("C:/Users/pluie/Desktop/milanorToolSidefile/manhwa/의룡 -Team Medical Dragon-/medical_dragon01.zip");
+		byte[] image = null;
+		for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
+	        ZipEntry entry = (ZipEntry) e.nextElement();
+	        
+	        if (!entry.isDirectory()) {
+	        		image = getImage(zip.getInputStream(entry));
+	        }
+		}
+		return image;
+	}
+	
+//	public static String Zip_tools_text () throws IOException {
+//		ZipFile zip = new ZipFile("C:/Users/pluie/Desktop/milanorToolSidefile/manhwa/의룡 -Team Medical Dragon-/medical_dragon01.zip");
+//	
+//		for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
+//	        ZipEntry entry = (ZipEntry) e.nextElement();
+//	        
+//	        if (!entry.isDirectory()) {
+//	        	
+//	            if (FilenameUtils.getExtension(entry.getName()).equals("jpg")) {
+//	            	
+//	                byte[] image = getImage(zip.getInputStream(entry));
+//
+//	                	return image;
+//	                
+//	            } else if (FilenameUtils.getExtension(entry.getName()).equals("txt")) {
+//	                StringBuilder out = getTxtFiles(zip.getInputStream(entry));
+//	                
+//	                return out;
+//	            }
+//	        }
+//	    }
+//	}
+	
+	//image 파일 컨트롤
+	private static byte[] getImage(InputStream in)  {
+	    try {
+	        BufferedImage image = ImageIO.read(in); //just checking if the InputStream belongs in fact to an image
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        ImageIO.write(image, "jpg", baos);
+	        return baos.toByteArray();
+	    } catch (IOException e) {
+	        // do something, it is not a image
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	
+	//text 파일 컨트롤
+	private  static StringBuilder getTxtFiles(InputStream in)  {
+	    StringBuilder out = new StringBuilder();
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+	    String line;
+	    try {
+	        while ((line = reader.readLine()) != null) {
+	            out.append(line);
+	        }
+	    } catch (IOException e) {
+	        // do something, probably not a text file
+	        e.printStackTrace();
+	    }
+	    return out;
+	}
 }
+
+
